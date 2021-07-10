ecd48e4765d3497c1f3e808596a3e998cbd1ee40
Thomas Piotaix <thomas.piotaix@gmail.com>
2021-07-10 13:07:57 +0200

    Avec la pallette et les couleurs

--- main.cpp
+++ main.cpp
@@ -24,19 +24,55 @@ complex<double> get_C_number_from_pixel(int i, int j, const Mat *image)
   return result;
 }
 
-Vec3f get_color_from_nb_iteration(int nb, int nb_max)
+Vec3f get_color_from_nb_iteration(int nb)
 {
-  float value;
-  if ((nb % 100) < 50)
+  float value_r;
+  float value_g;
+  float value_b;
+  if ((nb % 300) < 50)
   {
-    value = (nb % 50) / 50.0;
+    //Dégradé de noir vers blanc
+    value_r = (nb % 50) / 50.0;
+    value_g = (nb % 50) / 50.0;
+    value_b = (nb % 50) / 50.0;
   }
-  else
+  else if ((nb % 300) < 100)
   {
-    value = 1 - (nb % 50) / 50.0;
+    //Dégradé de blanc vers bleu
+    value_r = 1 - (nb % 50) / 50.0;
+    value_g = 1 - (nb % 50) / 50.0;
+    value_b = 1;
+  }
+  else if ((nb % 300) < 150)
+  {
+    //Dégradé de bleu vers violet
+    value_r = (nb % 50) / 50.0;
+    value_g = 0;
+    value_b = 1;
+  }
+  else if ((nb % 300) < 200)
+  {
+    //Dégradé de violet vers rouge
+    value_r = 1;
+    value_g = 0;
+    value_b = 1 - (nb % 50) / 50.00;
+  }
+  else if ((nb % 300) < 250)
+  {
+    //Dégradé de rouge vers orange
+    value_r = 1;
+    value_g = (165 / 255.0) * (nb % 50) / 50.0;
+    value_b = 0;
+  }
+  else if ((nb % 300) < 300)
+  {
+    //Dégradé de orange vers noir
+    value_r = 1 - (nb % 50) / 50.00;
+    value_g = (165 / 255.0) - (165 / 255.0) * (nb % 50) / 50.0;
+    value_b = 0;
   }
 
-  Vec3f intensity(value, value, value);
+  Vec3f intensity(value_b, value_g, value_r);
   // Vec3f intensity = img.at<Vec3f>(j, i);
   // cout << intensity << endl;
   // float blue = intensity.val[0];
@@ -53,25 +89,55 @@ Vec3f color_of_C_number(complex<double> z, int nb_iteration_max)
     z = z * z + c;
     if (abs(z) > 2)
     {
-      return get_color_from_nb_iteration(i, nb_iteration_max);
+      return get_color_from_nb_iteration(i);
     }
   }
   Vec3f intensity(0, 0, 0);
   return intensity;
 }
 
+void make_pallette()
+{
+  int dim_x = 1000;
+  int dim_y = 200;
+  Mat pallette = Mat::ones(Size(dim_x, dim_y), CV_32FC3);
+  pallette.convertTo(pallette, CV_32FC3, 1 / 255.0); //Image avec trois channels B,G,R codés entre 0 et 1
+#pragma omp parallel for schedule(dynamic, 1)
+  for (int j = 0; j < pallette.cols; j++)
+  {
+    Vec3f color = get_color_from_nb_iteration(j);
+    for (int i = 0; i < pallette.rows; i++)
+    {
+      pallette.at<Vec3f>(i, j) = color;
+    }
+  }
+  display(pallette);
+}
+
 int main()
 {
-  int nb_iterations = 1000;
+  int nb_iterations;
   int NTHREADS;
+  int make_pal;
+  cout << "Merci de ne pas faire l'autiste et de mettre des entiers." << endl;
+  cout << "Nombre d'itérations ?" << endl;
+  cin >> nb_iterations;
   cout << "Nombre de threads ?" << endl;
   cin >> NTHREADS;
+  cout << "Faire la pallette ? 0 pour non, 1 pour oui." << endl;
+  cin >> make_pal;
 
   Mat img = imread("resources/base10k.png");
   img.convertTo(img, CV_32F, 1 / 255.0); //Image avec trois channels B,G,R codés entre 0 et 1
-  
+
   omp_set_num_threads(NTHREADS);
   auto start = omp_get_wtime();
+
+  if (make_pal == 1)
+  {
+    make_pallette();
+    return 0;
+  }
 
 #pragma omp parallel for schedule(dynamic, 1)
   for (int i = 0; i < img.rows; i++)
